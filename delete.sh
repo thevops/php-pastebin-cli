540459c1b018e91bf3fddd802ff7f6bd505733c3
Krzysztof Luczak <krzysztof.luczak.pro@gmail.com>
2020-04-03 23:52:11 +0200

    next

--- delete.sh
+++ delete.sh
@@ -10,7 +10,7 @@ current_datetime="$(date +%s)"
 cat "$delete_file" | while IFS= read line
 do
     file="$(echo $line | awk '{print $1}')"
-    datetime="$(echo $line | awk '{print $2 $3}')"
+    datetime="$(echo $line | awk '{print $2 " " $3}')"
     delete_time=$(date --date="$datetime" +%s)
 
     let "time_diff=$delete_time-$current_datetime"
