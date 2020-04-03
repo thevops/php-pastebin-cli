0a48daaff32193864615824cb3ec2c8a745383ff
Krzysztof Luczak <krzysztof.luczak.pro@gmail.com>
2020-04-03 23:24:11 +0200

    next

--- extended.php
+++ extended.php
@@ -21,20 +21,21 @@
         $date = date('Y-m-d_H:i');
         if(isset($_GET['d'])) {
             $time = $_GET['d'];
-            $delete_date = date('Y-m-d_H:i', strtotime($date . ' + ' . $time . ' days'));
+            $delete_date = date('Y-m-d_H:i', strtotime(' + ' . $time . ' days'));
             file_put_contents('delete.txt', $randomName . ' ' . $delete_date . "\n", FILE_APPEND | LOCK_EX);
         } 
         elseif(isset($_GET['m'])) {
             $time = $_GET['m'];
-            $delete_date = date('Y-m-d_H:i', strtotime($date . ' + ' . $time . ' minutes'));
+            $delete_date = date('Y-m-d_H:i', strtotime(' + ' . $time . ' minutes'));
             file_put_contents('delete.txt', $randomName . ' ' . $delete_date . "\n", FILE_APPEND | LOCK_EX);
         } 
         else {
             echo "Invalid syntax";
             die();
         }
-        echo $date;
-        echo $delete_date;
+        echo $time . "\n";
+        echo $date . "\n";
+        echo $delete_date . "\n";
 
         $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['PHP_SELF']);
         echo "$link" . "/n/" . "$randomName" . "\n";
